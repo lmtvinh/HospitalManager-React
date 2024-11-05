d8aec0d9040418aa6709c1010d063d32a9fdc04b
Hữu Hoàng <huuhoag1412@gmail.com>
2024-11-06 00:13:31 +0700

    fix old router

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,36 +1,30 @@
-import { useNavigate } from "@tanstack/react-router";
-import { Button as BootstrapBtn } from "react-bootstrap";
+import { Button as BootstrapBtn } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
-interface ButtonProps extends React.ComponentProps<typeof BootstrapBtn>{
-    href?: string;
-    icon?: React.ReactNode;
+interface ButtonProps extends React.ComponentProps<typeof BootstrapBtn> {
+  href?: string;
+  icon?: React.ReactNode;
 }
 
-function Button({ href, children, icon,onClick, ...props }: ButtonProps) {
-    
-    const navigate = useNavigate();
-    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-        if (href) {
-            if(href.startsWith('#')){
-                window.location.hash = href;
-                return;
-            }
-            navigate({
-                to: href,
-            })
-        }
-        onClick && onClick(e);
+function Button({ href, children, icon, onClick, ...props }: ButtonProps) {
+  const navigate = useNavigate();
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (href) {
+      if (href.startsWith('#')) {
+        window.location.hash = href;
+        return;
+      }
+      navigate(href);
     }
+    onClick && onClick(e);
+  };
 
-    return (
-        <BootstrapBtn
-            {...props}
-            onClick={handleClick}
-        >
-            {children}
-            {icon}
-        </BootstrapBtn>
-    );
+  return (
+    <BootstrapBtn {...props} onClick={handleClick}>
+      {children}
+      {icon}
+    </BootstrapBtn>
+  );
 }
 
 export default Button;
